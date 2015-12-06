e995dcde971ba4d5d928451b0ff1521b8cf54452
Joe Karlsson <joe.carlson@callmejoe.net>
2015-12-06 08:33:31 -1000

    Got d3 working on the client side.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bechdel Test Visualizer' });
+  res.render('index', {
+    title : 'Bechdel Test Visualizer'
+  });
+
 });
 
 module.exports = router;
