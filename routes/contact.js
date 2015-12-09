73ef74bb9293d2d28a41e2235159371add7a974e
Joe Karlsson <joe.carlson@callmejoe.net>
2015-12-09 12:26:21 -1000

    gulpfile

---
+++ routes/contact.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET Contact page. */
+router.get('/', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+module.exports = router;
