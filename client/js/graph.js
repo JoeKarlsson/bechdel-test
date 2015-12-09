a93afa66e9d0e5ca775c054700631f0289ebbe1b
Joe Karlsson <joe.carlson@callmejoe.net>
2015-12-08 17:04:35 -1000

    Updated readme.

--- client/js/graph.js
+++ client/js/graph.js
@@ -1,27 +1,51 @@
-var data = [4, 8, 15, 16, 23, 42];
+var chart = c3.generate({
+  data : {
+    columns : [
+        ['data1', 30],
+        ['data2', 120]
+    ],
+    type : 'donut',
+    onclick : function (d, i) { console.log('onclick', d, i); },
+    onmouseover : function (d, i) { console.log('onmouseover', d, i); },
+    onmouseout : function (d, i) { console.log('onmouseout', d, i); }
+  },
+  donut : {
+    title : 'Iris Petal Width'
+  }
+});
 
-var width = 420,
-    barHeight = 20;
+setTimeout(function () {
+  chart.load({
+    columns : [
+        ['setosa', 0.2, 0.2, 0.2, 0.2, 0.2, 0.4, 0.3, 0.2, 0.2, 0.1, 0.2, 0.2, 0.1, 0.1, 0.2, 0.4, 0.4, 0.3, 0.3, 0.3, 0.2, 0.4, 0.2, 0.5, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.2, 0.4, 0.1, 0.2, 0.2, 0.2, 0.2, 0.1, 0.2, 0.2, 0.3, 0.3, 0.2, 0.6, 0.4, 0.3, 0.2, 0.2, 0.2, 0.2],
+        ['versicolor', 1.4, 1.5, 1.5, 1.3, 1.5, 1.3, 1.6, 1.0, 1.3, 1.4, 1.0, 1.5, 1.0, 1.4, 1.3, 1.4, 1.5, 1.0, 1.5, 1.1, 1.8, 1.3, 1.5, 1.2, 1.3, 1.4, 1.4, 1.7, 1.5, 1.0, 1.1, 1.0, 1.2, 1.6, 1.5, 1.6, 1.5, 1.3, 1.3, 1.3, 1.2, 1.4, 1.2, 1.0, 1.3, 1.2, 1.3, 1.3, 1.1, 1.3],
+        ['virginica', 2.5, 1.9, 2.1, 1.8, 2.2, 2.1, 1.7, 1.8, 1.8, 2.5, 2.0, 1.9, 2.1, 2.0, 2.4, 2.3, 1.8, 2.2, 2.3, 1.5, 2.3, 2.0, 2.0, 1.8, 2.1, 1.8, 1.8, 1.8, 2.1, 1.6, 1.9, 2.0, 2.2, 1.5, 1.4, 2.3, 2.4, 1.8, 1.8, 2.1, 2.4, 2.3, 1.9, 2.3, 2.5, 2.3, 1.9, 2.0, 2.3, 1.8]
+    ]
+  });
+}, 1500);
 
-var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, width]);
+setTimeout(function () {
+  chart.unload({
+    ids : 'data1'
+  });
+  chart.unload({
+    ids : 'data2'
+  });
+}, 2500);
 
-var chart = d3.select('.chart')
-    .attr('width', width)
-    .attr('height', barHeight * data.length);
 
-var bar = chart.selectAll('g')
-    .data(data)
-  .enter().append('g')
-    .attr('transform', function(d, i) { return 'translate(0,' + i * barHeight + ')'; });
+//////////////////////
 
-bar.append('rect')
-    .attr('width', x)
-    .attr('height', barHeight - 1);
 
-bar.append('text')
-    .attr('x', function(d) { return x(d) - 3; })
-    .attr('y', barHeight / 2)
-    .attr('dy', '.35em')
-    .text(function(d) { return d; });
+// var charttwo = c3.generate({
+//     data: {
+//         columns: [
+//             ['data1', 300, 350, 300, 0, 0, 0],
+//             ['data2', 130, 100, 140, 200, 150, 50]
+//         ],
+//         types: {
+//             data1: 'area',
+//             data2: 'area-spline'
+//         }
+//     }
+// });
