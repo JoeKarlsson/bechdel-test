aceddbebc962a1e910a2865873d5c409d1e66f1a
Joe Karlsson <joe.carlson@callmejoe.net>
2015-12-11 20:56:08 -1000

    Better error handling

--- methods/omdb.js
+++ methods/omdb.js
@@ -4,6 +4,9 @@ var request = require('request');
 //Token for http://api.myapifilms.com/imdb.do
 var myapifilmstoken = 'd44147a7-5e6e-4450-92ba-773be44791ce';
 
+// Sample complete URL call - used for troubleshooting api
+// http://api.myapifilms.com/imdb/idIMDB?title=gone+girl&token=d44147a7-5e6e-4450-92ba-773be44791ce&format=json&language=en-us&aka=0&business=0&seasons=0&seasonYear=0&technical=0&filter=3&exactFilter=0&limit=1&forceYear=0&trailers=0&movieTrivia=0&awards=0&moviePhotos=0&movieVideos=0&actors=1&biography=1&uniqueName=0&filmography=0&bornAndDead=0&starSign=0&actorActress=1&actorTrivia=0&similarMovies=0&adultSearch=0
+
 var movieCharacters = [];
 
 module.exports.getOmdbData = function (movieTitle) {
@@ -19,7 +22,7 @@ module.exports.getOmdbData = function (movieTitle) {
       .then(function (data) {
         return omdbFullCast(splitTitle)
           .then(function () {
-            console.log('Finished retrieving all data from OMDB..');
+            console.log('Finished retrieving all data from myapifilms..');
             resolve(movieCharacters);
           }), function (error) {
             // If there's an error or a non-200 status code, log the error.
@@ -34,7 +37,7 @@ module.exports.getOmdbData = function (movieTitle) {
 
 function omdbSimpleCast(splitTitle) {
   return Q.promise(function (resolve, reject) {
-    console.log('Started phase I - Retrieving Simple movie data from OMDB...');
+    console.log('Started phase I - Retrieving simple movie data via myapifilms...');
 
     request('http://api.myapifilms.com/imdb/idIMDB?title=' + splitTitle + '&token=' + myapifilmstoken + '&format=json&language=en-us&aka=0&business=0&seasons=0&seasonYear=0&technical=0&filter=3&exactFilter=0&limit=1&forceYear=0&trailers=0&movieTrivia=0&awards=0&moviePhotos=0&movieVideos=0&actors=1&biography=1&uniqueName=0&filmography=0&bornAndDead=0&starSign=0&actorActress=1&actorTrivia=0&similarMovies=0&adultSearch=0', function (error, response, body) {
       if (!error && response.statusCode == 200) {
@@ -48,7 +51,7 @@ function omdbSimpleCast(splitTitle) {
 
 function omdbFullCast(splitTitle) {
   return Q.promise(function (resolve, reject) {
-    console.log('Started Phase II - Retreiving full character data from OMDB..');
+    console.log('Started Phase II - Retreiving full character data from myapifilms..');
 
     request('http://api.myapifilms.com/imdb/idIMDB?title=' + splitTitle + '&token=' + myapifilmstoken + '&format=json&language=en-us&aka=0&business=0&seasons=0&seasonYear=0&technical=0&filter=3&exactFilter=0&limit=1&forceYear=0&trailers=0&movieTrivia=0&awards=0&moviePhotos=0&movieVideos=0&actors=2&biography=1&uniqueName=0&filmography=0&bornAndDead=0&starSign=0&actorActress=1&actorTrivia=0&similarMovies=0&adultSearch=0',
     function (error, response, body) {
@@ -67,21 +70,34 @@ function omdbDataParser(body) {
   }
 
   movieData = JSON.parse(body); // Show the request for the omdb api.
-  rawMovieCharacters = movieData.data.movies[0].actors;
 
-  //Save character/actor & actress data to the movieCharacters array
-  var charObj;
-  for (var i = 0; i < rawMovieCharacters.length; i++) {
-    //TODO Format char names - still has 'aroun some of the names'
-    var characterNameFormatted = rawMovieCharacters[i].character.replace(/'([^']+(?='))'/g, '$1').toUpperCase();
+  if ( "actors" in movieData.data ) {
 
-    //.replace(/\\/g, '')
+    console.log(movieData.data.movies[0])
+    rawMovieCharacters = movieData.data.movies[0].actors;
 
-    movieCharacters.push({
-      'actorName' : rawMovieCharacters[i].actorName,
-      'gender' : rawMovieCharacters[i].biography.actorActress,
-      'characterName' : characterNameFormatted
-    });
+    //Save character/actor & actress data to the movieCharacters array
+    var charObj;
+    for (var i = 0; i < rawMovieCharacters.length; i++) {
+      //TODO Format char names - still has 'around some of the names'
+      var characterNameFormatted = rawMovieCharacters[i].character.replace(/'([^']+(?='))'/g, '$1').toUpperCase();
+
+      //If a char is missing biography info - skip this character
+      if ( characterNameFormatted = '') {
+        if ( "biography" in rawMovieCharacters[i] ) {
+          // console.log(rawMovieCharacters[i].actorName)
+          movieCharacters.push({
+            'actorName' : rawMovieCharacters[i].actorName,
+            'gender' : rawMovieCharacters[i].biography.actorActress,
+            'characterName' : characterNameFormatted
+          });
+        }
+
+      }
+    }
+
+  } else {
+    console.error('Error: Connected to myfilmapi, but no actor data returned');
   }
 
   //Returns an array of movie characters with gender data
