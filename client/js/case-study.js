ddc520b0d239efb9a22b7f4ee612b62e99ef4555
Joe Karlsson <joe.carlson@callmejoe.net>
2015-12-15 08:49:36 -1000

    added case study graph files.

---
+++ client/js/case-study.js
@@ -0,0 +1,151 @@
+var grandTotalDonut = c3.generate({
+  bindto : '#grandTotalDonut',
+  data : {
+    columns : [
+        ['Passed', 38.46],
+        ['Failed', 61.54]
+    ],
+    type : 'donut',
+    onclick : function (d, i) { console.log('onclick', d, i); },
+    onmouseover : function (d, i) { console.log('onmouseover', d, i); },
+    onmouseout : function (d, i) { console.log('onmouseout', d, i); }
+  },
+  color : {
+    pattern : ['#43AC6A', '#F14A52']
+  }
+});
+
+//////////////////////
+
+var bestPictureDonut = c3.generate({
+  bindto : '#bestPictureDonut',
+  data : {
+    columns : [
+        ['Passed', 28.57],
+        ['Failed', 71.43]
+    ],
+    type : 'donut',
+    onclick : function (d, i) { console.log('onclick', d, i); },
+    onmouseover : function (d, i) { console.log('onmouseover', d, i); },
+    onmouseout : function (d, i) { console.log('onmouseout', d, i); }
+  },
+  color : {
+    pattern : ['#43AC6A', '#F14A52']
+  }
+});
+
+//////////////////////
+
+var bestActorDonut = c3.generate({
+  bindto : '#bestActorDonut',
+  data : {
+    columns : [
+        ['Passed', 20],
+        ['Failed', 80]
+    ],
+    type : 'donut',
+    onclick : function (d, i) { console.log('onclick', d, i); },
+    onmouseover : function (d, i) { console.log('onmouseover', d, i); },
+    onmouseout : function (d, i) { console.log('onmouseout', d, i); }
+  },
+  color : {
+    pattern : ['#43AC6A', '#F14A52']
+  }
+});
+
+//////////////////////
+
+var bestSupportingActorDonut = c3.generate({
+  bindto : '#bestSupportingActorDonut',
+  data : {
+    columns : [
+        ['Passed', 20],
+        ['Failed', 80]
+    ],
+    type : 'donut',
+    onclick : function (d, i) { console.log('onclick', d, i); },
+    onmouseover : function (d, i) { console.log('onmouseover', d, i); },
+    onmouseout : function (d, i) { console.log('onmouseout', d, i); }
+  },
+  color : {
+    pattern : ['#43AC6A', '#F14A52']
+  }
+});
+
+//////////////////////
+
+var bestSupportingActorAndDonut = c3.generate({
+  bindto : '#bestSupportingActorAndDonut',
+  data : {
+    columns : [
+        ['Passed', 25],
+        ['Failed', 75]
+    ],
+    type : 'donut',
+    onclick : function (d, i) { console.log('onclick', d, i); },
+    onmouseover : function (d, i) { console.log('onmouseover', d, i); },
+    onmouseout : function (d, i) { console.log('onmouseout', d, i); }
+  },
+  color : {
+    pattern : ['#43AC6A', '#F14A52']
+  }
+});
+
+//////////////////////
+
+var bestActressDonut = c3.generate({
+  bindto : '#bestActressDonut',
+  data : {
+    columns : [
+        ['Passed', 50],
+        ['Failed', 50]
+    ],
+    type : 'donut',
+    onclick : function (d, i) { console.log('onclick', d, i); },
+    onmouseover : function (d, i) { console.log('onmouseover', d, i); },
+    onmouseout : function (d, i) { console.log('onmouseout', d, i); }
+  },
+  color : {
+    pattern : ['#43AC6A', '#F14A52']
+  }
+});
+
+//////////////////////
+
+var bestSupportingActressDonut = c3.generate({
+  bindto : '#bestSupportingActressDonut',
+  data : {
+    columns : [
+        ['Passed', 60],
+        ['Failed', 40]
+    ],
+    type : 'donut',
+    onclick : function (d, i) { console.log('onclick', d, i); },
+    onmouseover : function (d, i) { console.log('onmouseover', d, i); },
+    onmouseout : function (d, i) { console.log('onmouseout', d, i); }
+  },
+  color : {
+    pattern : ['#43AC6A', '#F14A52']
+  }
+});
+
+//////////////////////
+
+var bestSupportingActressAndDonut = c3.generate({
+  bindto : '#bestSupportingActressAndDonut',
+  data : {
+    columns : [
+        ['Passed', 62.5],
+        ['Failed', 37.5]
+    ],
+    type : 'donut',
+    onclick : function (d, i) { console.log('onclick', d, i); },
+    onmouseover : function (d, i) { console.log('onmouseover', d, i); },
+    onmouseout : function (d, i) { console.log('onmouseout', d, i); }
+  },
+  color : {
+    pattern : ['#43AC6A', '#F14A52']
+  }
+});
+
+//////////////////////
