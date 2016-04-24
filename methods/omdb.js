af4da3c9edcc7d2abea058e8172d55bbac29e64d
Joe Karlsson <joe.carlson@callmejoe.net>
2016-04-23 16:11:37 -1000

    refactored and removed DB

--- methods/omdb.js
+++ methods/omdb.js
@@ -1,15 +1,14 @@
-var Q = require('q'); //A tool for creating and composing asynchronous promises
+'use strict'
+
+var Q = require('q');
 var request = require('request');
 
 //Token for http://api.myapifilms.com/imdb.do
 var myapifilmstoken = 'd44147a7-5e6e-4450-92ba-773be44791ce';
 
-// Sample complete URL call - used for troubleshooting api
-// http://api.myapifilms.com/imdb/idIMDB?title=gone+girl&token=d44147a7-5e6e-4450-92ba-773be44791ce&format=json&language=en-us&aka=0&business=0&seasons=0&seasonYear=0&technical=0&filter=3&exactFilter=0&limit=1&forceYear=0&trailers=0&movieTrivia=0&awards=0&moviePhotos=0&movieVideos=0&actors=1&biography=1&uniqueName=0&filmography=0&bornAndDead=0&starSign=0&actorActress=1&actorTrivia=0&similarMovies=0&adultSearch=0
-
 var movieCharacters = [];
 
-module.exports.getOmdbData = function (movieTitle) {
+module.exports.getOmdbData = (movieTitle) => {
   return Q.promise(function (resolve, reject) {
 
     if (movieTitle === '') {
@@ -26,20 +25,20 @@ module.exports.getOmdbData = function (movieTitle) {
             resolve(movieCharacters);
           }), function (error) {
             // If there's an error or a non-200 status code, log the error.
-            console.error(error);
+            throw new Error(error);
           }
       }, function (error) {
           // If there's an error or a non-200 status code, log the error.
-          console.error(error);
+          throw new Error(error);
         })
   })
 }
 
-function omdbSimpleCast(splitTitle) {
+let omdbSimpleCast = ( splitTitle ) => {
   return Q.promise(function (resolve, reject) {
     console.log('Started phase I - Retrieving simple movie data via myapifilms...');
 
-    request('http://api.myapifilms.com/imdb/idIMDB?title=' + splitTitle + '&token=' + myapifilmstoken + '&format=json&language=en-us&aka=0&business=0&seasons=0&seasonYear=0&technical=0&filter=3&exactFilter=0&limit=1&forceYear=0&trailers=0&movieTrivia=0&awards=0&moviePhotos=0&movieVideos=0&actors=1&biography=1&uniqueName=0&filmography=0&bornAndDead=0&starSign=0&actorActress=1&actorTrivia=0&similarMovies=0&adultSearch=0', function (error, response, body) {
+    request('http://api.myapifilms.com/imdb/idIMDB?title=' + splitTitle + '&token=' + myapifilmstoken + '&format=json&language=en-us&aka=0&business=0&seasons=0&seasonYear=0&technical=0&filter=3&exactFilter=0&limit=1&forceYear=0&trailers=0&movieTrivia=0&awards=0&moviePhotos=0&movieVideos=0&actors=1&biography=1&uniqueName=0&filmography=0&bornAndDead=0&starSign=0&actorActress=1&actorTrivia=0&similarMovies=0&adultSearch=0', (error, response, body) => {
       if (!error && response.statusCode == 200) {
         resolve(omdbDataParser(body, 'mainCast'));
       }  else {
@@ -49,7 +48,7 @@ function omdbSimpleCast(splitTitle) {
   })
 }
 
-function omdbFullCast(splitTitle) {
+let omdbFullCast = (splitTitle) => {
   return Q.promise(function (resolve, reject) {
     console.log('Started Phase II - Retreiving full character data from myapifilms..');
 
@@ -64,44 +63,44 @@ function omdbFullCast(splitTitle) {
   })
 }
 
-function omdbDataParser(body, inputType) {
-  if (body === undefined || null || '' ) {
-    console.error('Body is undefined');
+let omdbDataParser = ( body, inputType ) => {
+  if ( body === undefined || null || '' ) {
+    console.error( 'Body is undefined' );
   }
 
-  movieData = JSON.parse(body); // Show the request for the omdb api.
+  // Show the request for the omdb api.
+  let movieData = JSON.parse( body );
 
-  rawMovieCharacters = movieData.data.movies[0].actors;
+  let rawMovieCharacters = movieData.data.movies[0].actors;
 
   if ( rawMovieCharacters !== undefined || null || '' ) {
 
+    //Save character/actor & actress data to the movieCharacters array
+    let charObj;
+    for (var i = 0; i < rawMovieCharacters.length; i++) {
 
-  //Save character/actor & actress data to the movieCharacters array
-  var charObj;
-  for (var i = 0; i < rawMovieCharacters.length; i++) {
-    //TODO Format char names - still has 'around some of the names'
-    var characterNameFormatted = rawMovieCharacters[i].character.replace(/'([^']+(?='))'/g, '$1').toUpperCase();
+      let characterNameFormatted = rawMovieCharacters[i].character.replace(/'([^']+(?='))'/g, '$1').toUpperCase();
 
-    //If a char is missing biography info - skip this character
-    if ( characterNameFormatted !== '' || undefined || null) {
-      if ( 'biography' in rawMovieCharacters[i] ) {
+      //If a char is missing biography info - skip this character
+      if ( characterNameFormatted !== '' || undefined || null) {
+        if ( 'biography' in rawMovieCharacters[i] ) {
 
-        if ( inputType === 'fullCast') {
-          var castType = true;
-        } else {
-          castType = false
-        }
-        movieCharacters.push({
+          if ( inputType === 'fullCast') {
+            var castType = true;
+          } else {
+            castType = false
+          }
+          movieCharacters.push({
               'actorName' : rawMovieCharacters[i].actorName,
               'gender' : rawMovieCharacters[i].biography.actorActress,
               'characterName' : characterNameFormatted,
               'mainCast' : castType
             });
 
+        }
       }
-    }
 
-  }
+    }
 
   } else {
     console.error('Error: Connected to myfilmapi, but no actor data returned');
---
+++ methods/readScript.js
@@ -0,0 +1,122 @@
+'use strict'
+
+var fs = require('fs');
+var Q = require('q');
+var omdb = require('./omdb');
+var bechdel = require('./bechdel');
+
+//Test movie scripts
+var scriptPath = './scripts/american-sniper.txt';
+// var scriptPath = './scripts/birdman.txt';
+// var scriptPath = './scripts/boyhood.txt';
+// var scriptPath = './scripts/foxcatcher.txt';
+// var scriptPath = './scripts/gone-girl.txt';
+// var scriptPath = './scripts/grand-budapest-hotel.txt';
+// var scriptPath = './scripts/imitation-game.txt';
+// var scriptPath = './scripts/still-alice.txt';
+// var scriptPath = './scripts/taxi-driver.txt';
+// var scriptPath = './scripts/theory-of-everything.txt';
+// var scriptPath = './scripts/whiplash.txt';
+// var scriptPath = './scripts/american-hustle.txt';
+// var scriptPath = './scripts/into-the-woods.txt';
+// var scriptPath = './scripts/judge.txt';
+// var scriptPath = './scripts/wild.txt';
+
+let readMovieTitle = (path) => {
+  return Q.promise( (resolve, reject) => {
+    var rs = fs.createReadStream(path, { encoding : 'utf8' });
+    var acc = '';
+    var pos = 0;
+    var index;
+
+    rs.on('data', (chunk) => {
+      index = chunk.indexOf('\n');
+      acc += chunk;
+      index !== -1 ? rs.close() : pos += chunk.length;
+    })
+    .on('close', () => {
+      let movieTitle = acc.slice(0, pos + index);
+      resolve(movieTitle);
+    })
+    .on('error', (err) => {
+      reject(err);
+    })
+  });
+};
+
+let readScript = (path) => {
+  return Q.promise( (resolve, reject) => {
+    var rs = fs.createReadStream(path, { encoding : 'utf8' });
+    var movieScript = '';
+
+    rs.on('data', (chunk) => {
+      movieScript += chunk;
+    })
+      .on('close', () => {
+        // console.log(movieScript);
+        resolve(movieScript);
+      })
+      .on('error', (err) => {
+        reject(err);
+      })
+  });
+};
+
+//In order to test locally, you must uncomment this section. I have commented this out, because I was making too many API calls on the server and it was causing issues when testing.
+
+let readAndAnalyzeScript = () => {
+  let movieCharacters = [];
+  let movieScript = '';
+
+  readMovieTitle(scriptPath)
+  .then( (movieTitle) => {
+    // If the HTTP response returns 200 OK, log the response text.
+    console.log( 'Movie Script Title: ', movieTitle );
+    return omdb.getOmdbData( movieTitle )
+
+      .then( ( movieCharacters ) => {
+        movieCharacters = movieCharacters;
+        return readScript(scriptPath)
+
+          .then( ( movieScript ) => {
+            movieScript = movieScript;
+            return bechdel.extractScenes( movieCharacters, movieScript )
+
+              .then( ( sceneArray ) => {
+                return bechdel.sceneAnalysis( movieCharacters, sceneArray )
+                  .then( ( result ) => {
+
+                  }, ( error ) => {
+                    // If there's an error or a non-200 status code, log the error.
+                    throw new Error( error );
+                  })
+
+              }, (error) => {
+                // If there's an error or a non-200 status code, log the error.
+                throw new Error(error);
+              })
+
+          }, (error) => {
+            // If there's an error or a non-200 status code, log the error.
+            throw new Error(error);
+          })
+
+      }, (error) => {
+        // If there's an error or a non-200 status code, log the error.
+        throw new Error(error);
+      })
+
+  }, (error) => {
+    // If there's an error or a non-200 status code, log the error.
+    throw new Error(error);
+  })
+
+  .catch(function (error) {
+    // Handle any error from all above steps
+    throw new Error(error);
+  })
+
+  .done();
+}
+
+module.exports = readAndAnalyzeScript;
