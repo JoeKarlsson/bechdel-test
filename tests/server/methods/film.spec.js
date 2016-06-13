5a4f7168b4a61c43be7725460d0ebc8eca0e0e77
Joe Karlsson <joe.carlson@callmejoe.net>
2016-06-12 18:41:02 -1000

    most tests are passing

--- tests/server/methods/film.spec.js
+++ tests/server/methods/film.spec.js
@@ -76,17 +76,18 @@ describe('Film methods', () => {
     const filmTitle = 'BOYHOOD';
     it('should call callback after success', () => {
       const req = sinon.stub(request, 'get');
-      const callback = sinon.spy();
       const response = JSON.stringify({ data: { movies: [{ imdbID: 1234 }] } });
       req.yields(null, null, response, 'mainCast');
-      film.getSimpleCastData(filmTitle, callback);
-      req.restore();
-      expect(callback).to.have.been.calledOnce;
+      film.getSimpleCastData(filmTitle)
+      .then((body) => {
+        req.restore();
+        expect(body).to.deep.equal(JSON.parse(response));
+      });
     });
     it('should send correct parameters to the expected URL', () => {
       const req = sinon.stub(request, 'get');
       const expectedUrl = 'http://api.myapifilms.com/imdb/idIMDB?title=BOYHOOD&token=d44147a7-5e6e-4450-92ba-773be44791ce&format=json&language=en-us&aka=0&business=0&seasons=0&seasonYear=0&technical=0&filter=3&exactFilter=0&limit=1&forceYear=0&trailers=0&movieTrivia=0&awards=0&moviePhotos=0&movieVideos=0&actors=1&biography=1&uniqueName=0&filmography=0&bornAndDead=0&starSign=0&actorActress=1&actorTrivia=0&similarMovies=0&adultSearch=0';
-      film.getSimpleCastData(filmTitle, () => {});
+      film.getSimpleCastData(filmTitle);
       req.restore();
       sinon.assert.calledWith(req, expectedUrl);
     });
@@ -112,9 +113,7 @@ describe('Film methods', () => {
       req.yields(null, null, response, 'fullCast');
       film.getFullCastData(filmTitle);
       sinon.assert.calledWith(req, expectedUrl);
-      // .then((body) => {
-      //   req.restore();
-      // });
+      req.restore();
     });
   });
 });
