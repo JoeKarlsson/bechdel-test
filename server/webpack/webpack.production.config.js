2571d0ec6de32aad0e4ef17a6da2561a1a60cbc7
Joe Karlsson <joe.carlson@callmejoe.net>
2016-10-03 13:04:58 -1000

    removed react-hmr from webpack config

--- server/webpack/webpack.production.config.js
+++ server/webpack/webpack.production.config.js
@@ -41,7 +41,7 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'babel',
       query: {
-        presets: ['react', 'es2015', 'stage-0', 'react-hmre'],
+        presets: ['react', 'es2015', 'stage-0'],
       },
     }, {
       test: /\.json?$/,
