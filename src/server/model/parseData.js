32c70ebca911565523d4198ae590320912137f5e
Joe Karlsson <josephcarlson1@gmail.com>
2017-12-18 15:27:00 -0600

    parse data refactored.

---
+++ src/server/model/parseData.js
@@ -0,0 +1,30 @@
+const parseActorArr = (arr) => {
+	if (!arr) {
+		throw new Error('Cannot parseActorArr');
+	}
+	const actorsArr = [];
+	let i;
+	for (i = 0; i < arr.length; i++) {
+		const actor = {};
+		actor.actorName = arr[i].actorName;
+		actor.character = arr[i].character;
+		actor.actorActress = arr[i].gender;
+		actorsArr.push(actor);
+	}
+	return actorsArr;
+};
+
+const parseImageData = (images) => {
+	if (!images) {
+		throw new Error('Cannot parseImageData');
+	}
+	const img = {};
+	img.backdrop = `https://image.tmdb.org/t/p/w1000${images.backdrops[0].file_path}`;
+	img.poster = `https://image.tmdb.org/t/p/w300${images.posters[0].file_path}`;
+	return img;
+};
+
+module.exports = {
+	parseActorArr,
+	parseImageData,
+};
---
+++ src/server/model/parseData.spec.js
@@ -0,0 +1,48 @@
+const parseData = require('./parseData');
+
+describe('Parse Data', () => {
+	describe('#parseActorArr', () => {
+		it('should parse Actor Array data', () => {
+			const mockActorArr = [
+				{
+					actorName: 'Joe',
+					character: 'Mike Bonchez',
+					gender: 'actor',
+				}, {
+					actorName: 'Laurel',
+					character: 'Wonder Woman',
+					gender: 'actress',
+				},
+			];
+			const expectedResult = [
+				{
+					actorName: 'Joe',
+					character: 'Mike Bonchez',
+					actorActress: 'actor',
+				}, {
+					actorName: 'Laurel',
+					character: 'Wonder Woman',
+					actorActress: 'actress',
+				},
+			];
+			const result = parseData.parseActorArr(mockActorArr);
+			expect(result).toMatchObject(expectedResult);
+		});
+	});
+
+	describe('#parseImageData', () => {
+		it('should parse Image Array data', () => {
+			const mockImageObj = {
+				backdrops: [{ file_path: '/test-backdrop' }],
+				posters: [{ file_path: '/test-poster' }],
+			};
+
+			const expectedResult = {
+				backdrop: 'https://image.tmdb.org/t/p/w1000/test-backdrop',
+				poster: 'https://image.tmdb.org/t/p/w300/test-poster',
+			};
+			const result = parseData.parseImageData(mockImageObj);
+			expect(result).toMatchObject(expectedResult);
+		});
+	});
+});
