136e8ef39e2c599fd753dfe5ae498cdd249456bb
Joe Karlsson <josephcarlson1@gmail.com>
2017-12-21 15:51:38 -0600

    added modk film data.

--- src/server/methods/getFilmData/getFullCastData.spec.js
+++ src/server/methods/getFilmData/getFullCastData.spec.js
@@ -12,12 +12,38 @@ describe('getFullCastData', () => {
 
 	it('should call callback after success', () => {
 		const filmTitle = 'BOYHOOD';
-		const hostname = 'http://api.myapifilms.com/imdb/idIMDB';
-		const path = `?title=${filmTitle}&token=${
-			meta.MYAPIFILMS
-		}&format=json&language=en-us&aka=0&business=0&seasons=0&seasonYear=0&technical=0&filter=3&exactFilter=0&limit=1&forceYear=0&trailers=0&movieTrivia=0&awards=0&moviePhotos=0&movieVideos=0&actors=2&biography=1&uniqueName=0&filmography=0&bornAndDead=0&starSign=0&actorActress=1&actorTrivia=0&similarMovies=0&adultSearch=0`;
+		const URL =
+			'http://api.myapifilms.com/imdb/idIMDB?' +
+			`title=${filmTitle}&` +
+			`token=${meta.MYAPIFILMS}&` +
+			'format=json&' +
+			'language=en-us&' +
+			'aka=0&' +
+			'business=0&' +
+			'seasons=0&' +
+			'seasonYear=0&' +
+			'technical=0&' +
+			'filter=3&' +
+			'exactFilter=0&' +
+			'limit=1&' +
+			'forceYear=0&' +
+			'trailers=0&' +
+			'movieTrivia=0&' +
+			'awards=0&' +
+			'moviePhotos=0&' +
+			'movieVideos=0&' +
+			'actors=2&' +
+			'biography=1&' +
+			'uniqueName=0&' +
+			'filmography=0&' +
+			'bornAndDead=0&' +
+			'starSign=0&' +
+			'actorActress=1&' +
+			'actorTrivia=0&' +
+			'similarMovies=0&' +
+			'adultSearch=0';
 
-		fetchMock.mock(hostname + path, mockFullData);
+		fetchMock.mock(URL, mockFullData);
 
 		getFullCastData(filmTitle).then(body => {
 			expect(body).toMatchObject(mockFullData);
