a9017fd35c95528cc47be084535791cd8473ce2c
Joe Karlsson <josephcarlson1@gmail.com>
2018-01-05 13:19:30 -0600

    merged data metadata.

--- src/server/methods/getFilmData/__mocks__/mock-film-data.js
+++ src/server/methods/getFilmData/__mocks__/mock-film-data.js
@@ -1,10 +1,9 @@
-const mockGetSimpleCastData = require('./mock-simple-data.json');
-const mockGetFullCastData = require('./mock-full-data.json');
+const mockMergedData = require('./mock-merged-data');
 const mockActorData = require('./mock-actor-data.json');
 const mockImageData = require('./mock-image-data.json');
 
 module.exports = {
 	actors: mockActorData,
 	images: mockImageData,
-	metadata: [mockGetSimpleCastData, mockGetFullCastData],
+	metadata: mockMergedData,
 };
