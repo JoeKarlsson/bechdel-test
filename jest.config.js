6ed46de70f3e7423f1b41ea001f3b1374e1e889f
Joe Karlsson <josephcarlson1@gmail.com>
2018-01-10 16:35:10 -0600

    new uploader working and tests passing.

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,14 @@
 module.exports = {
-	setupFiles: [
-		'./jestsetup.js',
+	setupFiles: ['./jestsetup.js'],
+	collectCoverageFrom: ['src/**/*.js'],
+	coveragePathIgnorePatterns: [
+		'<rootDir>/preview',
+		'<rootDir>/src/app/entry',
+		'<rootDir>/src/server/server',
 	],
-	collectCoverageFrom: [
-		'src/**/*.js',
-	],
-	coverageReporters: [
-		'html',
-		'text-summary',
-		'text',
-		'json',
-		'lcov',
-	],
+	coverageReporters: ['html', 'text-summary', 'text', 'json', 'lcov'],
 	coverageDirectory: 'coverage',
-	moduleDirectories: [
-		'node_modules',
-		'src',
-	],
+	moduleDirectories: ['node_modules', 'src'],
 	coverageThreshold: {
 		global: {
 			branches: 0,
@@ -27,7 +19,8 @@ module.exports = {
 	},
 	testEnvironment: 'node',
 	moduleNameMapper: {
-		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+			'<rootDir>/__mocks__/fileMock.js',
 		'\\.(css|scss)$': 'identity-obj-proxy',
 	},
 };
