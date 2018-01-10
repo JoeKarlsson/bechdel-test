ab3238fd3738991a41675817e1b834268f6b25af
Joe Karlsson <josephcarlson1@gmail.com>
2018-01-10 09:45:39 -0600

    Tests passing without uploader.

--- src/app/static/Home/Home.spec.js
+++ src/app/static/Home/Home.spec.js
@@ -2,37 +2,53 @@ import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
 import Home from './Home';
+
+jest.mock('../../helper/api');
 
 configure({ adapter: new Adapter() });
 
 describe('Home', () => {
 	let wrapper;
+	let inst;
 
 	global.requestAnimationFrame = callback => {
 		setTimeout(callback, 0);
 	};
 
 	beforeEach(() => {
-		wrapper = shallow(<Home />);
+		wrapper = shallow(
+			<MemoryRouter>
+				<Home />
+			</MemoryRouter>
+		);
+		inst = wrapper.instance();
 	});
 
-	describe('rendering', () => {
+	describe('Home', () => {
 		describe('initial state', () => {
 			it('is rendered', () => {
-				const component = renderer.create(<Home />);
+				const component = renderer.create(
+					<MemoryRouter>
+						<Home />
+					</MemoryRouter>
+				);
 				const tree = component.toJSON();
 				expect(tree).toMatchSnapshot();
 			});
 
-			it('is rendered correctly', () => {
-				expect(wrapper).toHaveLength(1);
+			it('should not have any inital props', () => {
+				const initialProps = inst.props;
+				const expectedProps = {
+					children: <Home />,
+				};
+				expect(initialProps).toMatchObject(expectedProps);
 			});
-
-			it('should have correct inital instance', () => {
-				const initialInstance = wrapper.instance();
-				const expectedInstance = null;
-				expect(initialInstance).toBe(expectedInstance);
+			it('should not have any inital state', () => {
+				const initialState = inst.state;
+				const expectedState = {};
+				expect(initialState).toMatchObject(expectedState);
 			});
 		});
 	});
