242f8d0ef24ff4863d64fe7f228d15e4a9c0ad50
Joe Karlsson <joe.carlson@callmejoe.net>
2020-02-05 13:44:30 -0600

    refreshed config.

--- src/server/model/index.js
+++ src/server/model/index.js
@@ -3,6 +3,8 @@ const meta = require('../helper/meta');
 
 const { isDeveloping } = meta;
 
+// mongodb+srv://joeKarlsson:<password>@bechdel-lmhrw.mongodb.net/test?retryWrites=true&w=majority
+
 if (isDeveloping) {
 	mongoose.connect('mongodb://database/bechdelTest', {
 		useMongoClient: true,
@@ -30,3 +32,35 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 module.exports = db;
+
+
+const mongoose = require('mongoose');
+const userSchema = require('./userSchema.js');
+const User = mongoose.model('user', userSchema, 'user');
+
+async function createUser(username) {
+	return new User({
+		username,
+		created: Date.now(),
+	}).save();
+}
+
+async function findUser(username) {
+	return await User.findOne({ username });
+}
+
+(async () => {
+	const connector = mongoose.connect(connectionString);
+	const username = process.argv[2].split('=')[1];
+
+	let user = await connector.then(async () => {
+		return findUser(username);
+	});
+
+	if (!user) {
+		user = await createUser(username);
+	}
+
+	console.log(user);
+	process.exit(0);
+})();
