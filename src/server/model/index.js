5d6282ba79eee280440417f0073da2a225f0292c
Joe Karlsson <joe.carlson@callmejoe.net>
2020-02-05 14:07:10 -0600

    updated readme.

--- src/server/model/index.js
+++ src/server/model/index.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const meta = require('../helper/meta');
 
-const { isDeveloping } = meta;
+const { isDeveloping, MONGODB_URI } = meta;
 
 // mongodb+srv://joeKarlsson:<password>@bechdel-lmhrw.mongodb.net/test?retryWrites=true&w=majority
 
 if (isDeveloping) {
-	mongoose.connect('mongodb://database/bechdelTest', {
+	mongoose.connect(MONGODB_URI, {
 		useMongoClient: true,
 	});
 } else {
@@ -32,35 +32,3 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 module.exports = db;
-
-
-const mongoose = require('mongoose');
-const userSchema = require('./userSchema.js');
-const User = mongoose.model('user', userSchema, 'user');
-
-async function createUser(username) {
-	return new User({
-		username,
-		created: Date.now(),
-	}).save();
-}
-
-async function findUser(username) {
-	return await User.findOne({ username });
-}
-
-(async () => {
-	const connector = mongoose.connect(connectionString);
-	const username = process.argv[2].split('=')[1];
-
-	let user = await connector.then(async () => {
-		return findUser(username);
-	});
-
-	if (!user) {
-		user = await createUser(username);
-	}
-
-	console.log(user);
-	process.exit(0);
-})();
