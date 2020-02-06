66d0e6f99c136765e7c66e67fb53e572b1988657
Joe Karlsson <joe.carlson@callmejoe.net>
2020-02-05 21:11:14 -0600

    uppy working - API still busted tho.

--- src/app/films/NewFilm/Uploader/Uploader.js
+++ src/app/films/NewFilm/Uploader/Uploader.js
@@ -1,57 +1,69 @@
-import React from 'react';
-import Uppy, { XHRUpload, Dashboard } from 'uppy';
+import '@uppy/core/dist/style.css';
+import '@uppy/dashboard/dist/style.css';
+import React, { Component } from 'react';
+import { Uppy } from '@uppy/core';
+import { Dashboard, DragDrop, ProgressBar } from '@uppy/react';
+import XHRUpload from '@uppy/xhr-upload';
 import ErrorBoundary from '../../../shared/ErrorBoundary/ErrorBoundary';
-import './Uploader.scss';
 
-const uppy = Uppy({
-	meta: { type: 'script' },
-	restrictions: {
-		maxNumberOfFiles: 10,
-		maxFileSize: 1000000,
-		minNumberOfFiles: 1,
-		allowedFileTypes: ['text/plain/*'],
-	},
-	thumbnailGeneration: true,
-	autoProceed: true,
-});
+class Uploader extends Component {
+	constructor(props) {
+		super(props);
 
-uppy.use(XHRUpload, {
-	endpoint: '/api/film',
-	fieldName: 'script',
-	method: 'post',
-	formData: true,
-	limit: 10,
-	getResponseData: xhr => {
-		let response = JSON.parse(xhr.response);
-		response = response['0'];
-		console.log('response', response);
+		this.uppy = new Uppy({
+			id: 'uppy',
+			meta: { type: 'script' },
+			restrictions: {
+				maxNumberOfFiles: 10,
+				maxFileSize: 1000000,
+				minNumberOfFiles: 1,
+				allowedFileTypes: ['text/plain/*'],
+			},
+			thumbnailGeneration: true,
+			autoProceed: true,
+			debug: true,
+		}).use(XHRUpload, {
+			endpoint: '/api/film',
+			fieldName: 'script',
+			method: 'post',
+			formData: true,
+			limit: 10,
+			getResponseData: xhr => {
+				let response = JSON.parse(xhr.response);
+				response = response['0'];
+				console.log('response', response);
 
-		// window.location = `/film/${response._id}`;
-		// return {
-		// 	url: xhr.responseXML.querySelector('Location').textContent,
-		// };
-	},
-});
+				window.location = `/film/${response._id}`;
+				return {
+					url: xhr.responseXML.querySelector('Location').textContent,
+				};
+			},
+		});
+	}
+	componentWillUnmount() {
+		this.uppy.close();
+	}
 
-uppy.on('complete', result => {
-	console.log('successful files:', result.successful);
-	console.log('failed files:', result.failed);
-});
-
-uppy.on('upload-success', result => {
-	console.log(result);
-});
-
-uppy.run();
-
-const Uploader = () => {
-	return (
-		<div>
+	render() {
+		return (
 			<ErrorBoundary>
 				<Dashboard uppy={uppy} />
+
+				<DragDrop
+					uppy={this.uppy}
+					locale={{
+						strings: {
+							chooseFile: 'Boop a file',
+							orDragDrop: 'or yoink it here',
+						},
+					}}
+				/>
+
+				<h2>Progress Bar</h2>
+				<ProgressBar uppy={this.uppy} hideAfterFinish={false} />
 			</ErrorBoundary>
-		</div>
-	);
-};
+		);
+	}
+}
 
 export default Uploader;
