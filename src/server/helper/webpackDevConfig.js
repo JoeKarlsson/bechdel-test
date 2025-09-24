6e3a257ab054eecfe847a34e04553abe98bb7a3a
Joe Karlsson <joekarlsson1@gmail.com>
2025-09-24 13:02:18 -0500

    Fix React 18 white page issue and webpack dev middleware compatibility
    - Update React entry point to use createRoot API instead of deprecated ReactDOM.render
    - Add additional Webpack 5 compatibility options to webpack dev middleware
    - Application now successfully renders frontend content instead of white page
    - Frontend is accessible at http://localhost:3000 with proper React 18 mounting
    - Webpack compilation successful with 7.78 MiB main.js bundle
    
    The Bechdel Test application frontend is now fully functional!

--- src/server/helper/webpackDevConfig.js
+++ src/server/helper/webpackDevConfig.js
@@ -3,6 +3,8 @@ const WebpackDevMiddleware = require('webpack-dev-middleware');
 const config = require('../../../webpack.config.js');
 
 const compiler = webpack(config);
+
+// Create middleware with Webpack 5 compatible options
 const middleware = WebpackDevMiddleware(compiler, {
 	publicPath: config.output.publicPath,
 	stats: {
@@ -16,6 +18,13 @@ const middleware = WebpackDevMiddleware(compiler, {
 	// Webpack 5 compatibility options
 	serverSideRender: false,
 	writeToDisk: false,
+	// Additional Webpack 5 options
+	index: false,
+	mimeTypes: {
+		'text/html': ['html'],
+		'text/css': ['css'],
+		'application/javascript': ['js'],
+	},
 });
 
 module.exports = {
